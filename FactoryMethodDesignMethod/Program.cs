ce2d59f033fe6d32bc4d2d0b85551cceb52d56f9
pervin agayev <pervinagayev28@gmail.com>
2023-11-26 17:52:44 +0400

    finished

---
+++ FactoryMethodDesignMethod/Program.cs
@@ -0,0 +1,74 @@
+﻿namespace FactoryMethodDesignMethod
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+
+    public interface IButton
+    {
+        void Render();
+        void onClick();
+    }
+
+    public class WindowsButton : IButton
+    {
+        public void onClick()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Render()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+
+
+    public class HtmlButton : IButton
+    {
+        public void onClick()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Render()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public abstract class Dialog
+    {
+        public void Render()
+        {
+            //some
+        }
+
+        abstract public IButton creatButton();
+
+    }
+
+
+
+    public class WindowsDialog : Dialog
+    {
+        public override IButton creatButton()
+        {
+            return new WindowsButton();
+        }
+    }
+
+
+    public class WebDialog : Dialog
+    {
+        public override IButton creatButton()
+        {
+            return new HtmlButton();
+        }
+    }
+}
